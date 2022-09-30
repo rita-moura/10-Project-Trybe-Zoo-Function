ffad30b4cde685d97b41223357819bdaece14565
Rita Moura <ritamoura520@gmail.com>
2022-09-30 11:35:04 -0300

    corrigindo lint

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,5 +1,17 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
+const object = {
+  Tuesday: { open: 8, close: 6 },
+  Wednesday: { open: 8, close: 6 },
+  Thursday: { open: 10, close: 8 },
+  Friday: { open: 10, close: 8 },
+  Saturday: { open: 8, close: 10 },
+  Sunday: { open: 8, close: 8 },
+  Monday: { open: 0, close: 0 },
+};
+
 describe('Testes da função getOpeningHours', () => {
-  test()
+  test('retona objeto se não for passado parametro', () => {
+    expect(getOpeningHours()).toBe(object);
+  });
 });
