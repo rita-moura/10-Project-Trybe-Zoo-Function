04602785021d70eb7c4686c7f717b0d9f941cf0d
Rita Moura <ritamoura520@gmail.com>
2022-09-30 19:47:54 -0300

    Requisito 4 feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.some((verifyId) => verifyId === id));
 }
+isManager();
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((element) => element.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
 
 module.exports = { isManager, getRelatedEmployees };
