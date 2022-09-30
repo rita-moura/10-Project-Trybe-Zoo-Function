97ba88cb24d22738767de1b287d715eee57165b4
Rita Moura <ritamoura520@gmail.com>
2022-09-30 13:32:19 -0300

    Requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-
+  return data.employees.reduce((acc, name) => {
+    if (name.firstName === employeeName || name.lastName === employeeName) {
+      return name;
+    } return acc;
+  }, {});
 }
+getEmployeeByName();
 
 module.exports = getEmployeeByName;
