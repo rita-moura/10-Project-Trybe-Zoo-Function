9eca00afa24e4b7e83d2b3d0246022a298fa7b74
Rita Moura <ritamoura520@gmail.com>
2022-09-30 11:15:13 -0300

    Requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,21 +1,8 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// coloquei um id como parametro retorna [especies]
-// função deve poder receber varios parametros
-// retornar um array
-// retornar um array vazio se não receber nenhum id
-// buscar a chave species
-// dentro desse array deve ter os animais da mesma especie
-
 function getSpeciesByIds(...ids) {
-  const arraySpecie = [];
-  return data.species.forEach((element) => {
-    if (element.id === ids) {
-      return arraySpecie.push(species);
-    }
-  });
+  return data.species.filter((species) => ids.find((specie) => species.id === specie));
 }
-console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+getSpeciesByIds();
 
 module.exports = getSpeciesByIds;
