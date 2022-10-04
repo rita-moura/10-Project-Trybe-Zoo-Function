66cd2b87eae8cf5c8e31d2ae279ba7d0ccf9844a
Rita Moura <ritamoura520@gmail.com>
2022-10-04 11:46:49 -0300

    tentativa 10 novamente

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,14 +4,14 @@ const { species, employees } = data;
 
 function getOldestFromFirstSpecies(id) {
   const emplyeesId = employees.find((idEmplyees) => idEmplyees.id === id).responsibleFor[0];
-  // console.log('id do colaborador', emplyeesId);
+
   const animalsId = species.find((animalId) => emplyeesId === animalId.id)
     .residents.reduce((acc, curr) => {
       if (acc.age > curr.age) return acc;
       return curr;
     });
-  // console.log('chave e valor do animal mais velho que o colaborador cuida', animalsId);
+
   return Object.values(animalsId);
 }
-// console.log(getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+
 module.exports = getOldestFromFirstSpecies;
